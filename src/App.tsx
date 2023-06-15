8a9cdc7abba90d7c4eee7568204143d6b60020b2
novianto22 <99942803+novianto22@users.noreply.github.com>
2023-06-15 23:04:08 +0700

    feat: initial commit
    Setup react ts tailwindcss with husky, commitlint, commitizen and alliases import

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <div>
+            <h1 className="text-4xl">
+                React Typescript TailwindCSS Commitizen Starter Template
+            </h1>
+        </div>
+    );
+};
+
+export default App;
